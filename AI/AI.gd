c8e028068cc01fdc9db49420d7c9ca142018a5a5
Safirl <loic.leforestier@mmibordeaux.com>
2024-10-10 18:27:39 +0200

    Keep working on AI

--- AI/AI.gd
+++ AI/AI.gd
@@ -1,28 +1,15 @@
 extends CharacterBody3D
 
+var speed = 2.
+var accel = 10.
+@onready var NavigationAgent = $NavigationAgent3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+# Cette fonction est appelée chaque frame pour déplacer l'IA
+func _physics_process(delta):
+	NavigationAgent.target_position = get_parent().get_node("target").global_transform.origin
+	
+	var direction = (NavigationAgent.get_next_path_position() - global_position).normalized()
+	
+	velocity = velocity.lerp(direction * speed, accel * delta)
+	
 	move_and_slide()
