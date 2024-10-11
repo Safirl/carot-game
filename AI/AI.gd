9a306002f475583e2020215d9e791450e3194274
Safirl <loic.leforestier@mmibordeaux.com>
2024-10-11 10:00:02 +0200

    Fixing global AI

--- AI/AI.gd
+++ AI/AI.gd
@@ -3,10 +3,14 @@ extends CharacterBody3D
 var speed = 2.
 var accel = 10.
 @onready var NavigationAgent = $NavigationAgent3D
+@onready var target = owner.get_node("target")
 
 # Cette fonction est appelée chaque frame pour déplacer l'IA
 func _physics_process(delta):
-	NavigationAgent.target_position = get_parent().get_node("target").global_transform.origin
+	if !target:
+		pass
+		
+	NavigationAgent.target_position = target.global_transform.origin
 	
 	var direction = (NavigationAgent.get_next_path_position() - global_position).normalized()
 	
