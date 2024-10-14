8c9ae55fed4e4cf33a0d668f3b738a3a410a9d5d
Safirl <loic.leforestier@mmibordeaux.com>
2024-10-14 21:04:39 +0200

    adding farmer AI
    # Conflicts:
    #	AI/Ennemy/enemiIA.gd

--- AI/Ennemy/enemiIA.gd
+++ AI/Ennemy/enemiIA.gd
@@ -4,12 +4,12 @@ enum States { IDLE, CHASING, ATTACKING }
 var current_state = States.IDLE
 var state : String = "idle"
 var last_direction : String = "idle"
+var direction: Vector3
+@export var wander_radius: float = 10.
 
-var impulse_direction: Vector3
-var impulse: float = 3
 @export var gravity = 9.8
 
-var speed = 1.
+var speed = 1.5
 var accel = 1.
 @onready var NavigationAgent = $NavigationAgent3D
 @onready var anim_player = $AnimatedSprite3D
@@ -17,9 +17,6 @@ var accel = 1.
 
 # Cette fonction est appelée chaque frame pour déplacer l'IA
 func _physics_process(delta):
-	if !target:
-		return
-	NavigationAgent.target_position = target.global_transform.origin
 	match current_state:
 		States.IDLE:
 			_idle()
@@ -30,34 +27,8 @@ func _physics_process(delta):
 			
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-			
-func _chasing():
-	var direction: Vector3
-	if !global_transform.origin.distance_to(NavigationAgent.target_position) > NavigationAgent.target_desired_distance:
-		direction = Vector3.ZERO
-		velocity = Vector3.ZERO
-		current_state = States.IDLE
-	else:
-		direction = (NavigationAgent.get_next_path_position() - global_position).normalized()
-		velocity = velocity.lerp(direction * speed, accel * get_physics_process_delta_time())
-		move_and_slide()
-	
-	# Déterminer l'état en fonction de la direction
-	if direction != Vector3.ZERO:
-		if abs(direction.x) < abs(direction.z):
-			if direction.z > 0:
-				state = "walkup"
-			elif direction.z < 0:
-				state = "walkdown"
-		else:
-			if direction.x > 0:
-				state = "walkright"
-			elif direction.x < 0:
-				state = "walkleft"							
-		last_direction = state  # Met à jour la dernière direction
-	else:
-		state = "idle"
 	anim_player.play("walkingRight")
+		
 	#match state:
 		#"walkdown":
 			#anim_player.play("portaitFace")
@@ -78,10 +49,55 @@ func _chasing():
 					#anim_player.play("DefaultRight")
 				#"walkleft":
 					#anim_player.play("DefaultLeft")
+			
+func _chasing():
+	##Replace with nearest target
+	if !target:
+		return
+	NavigationAgent.target_position = target.global_transform.origin
+	##End of repalce
+	if !global_transform.origin.distance_to(NavigationAgent.target_position) > NavigationAgent.target_desired_distance:
+		direction = Vector3.ZERO
+		velocity = Vector3.ZERO
+		current_state = States.IDLE
+	else:
+		direction = (NavigationAgent.get_next_path_position() - global_position).normalized()
+		velocity = velocity.lerp(direction * speed, accel * get_physics_process_delta_time())
+		move_and_slide()
+	
+	# Déterminer l'état en fonction de la direction
+	if direction != Vector3.ZERO:
+		if abs(direction.x) < abs(direction.z):
+			if direction.z > 0:
+				state = "walkup"
+			elif direction.z < 0:
+				state = "walkdown"
+		else:
+			if direction.x > 0:
+				state = "walkright"
+			elif direction.x < 0:
+				state = "walkleft"
+		last_direction = state  # Met à jour la dernière direction
+	else:
+		state = "idle"
 	
 func _idle():
-	if !global_transform.origin.distance_to(NavigationAgent.target_position) < NavigationAgent.target_desired_distance:
-		current_state = States.CHASING
+	if global_transform.origin.distance_to(NavigationAgent.target_position) < NavigationAgent.target_desired_distance || NavigationAgent.target_position == Vector3.ZERO:
+		NavigationAgent.target_position = choose_random_destination()
+	var direction: Vector3
+	direction = (NavigationAgent.get_next_path_position() - global_position).normalized()
+	velocity = velocity.lerp(direction * speed, accel * get_physics_process_delta_time())
+	move_and_slide()
+	
 
 func _attacking():
 	pass
+
+func choose_random_destination() -> Vector3:
+	var random_destination = Vector3(
+		randf_range(-1.0, 1.0),
+		0, 
+		randf_range(-1.0, 1.0)
+	).normalized()  
+	random_destination = global_transform.origin + random_destination * wander_radius
+	return random_destination
